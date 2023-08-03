43eccf2b26a14a74fa8e1a62c7394a7855aaab66
akshay <akshaysajitha@gmail.com>
2023-08-03 12:20:19 +0530

    student.ts file check the value get from the search box if any match present store the data to foundstudent arry

--- src/app/student/student.component.ts
+++ src/app/student/student.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit,OnChanges } from '@angular/core';
-import student from '../student.json'
+import { Component, OnInit } from '@angular/core';
+import student from '../student.json';
 
 interface Student {
-  id: number|string;
+  id: number | string;
   name: string;
   address: string;
-  number: number|string;
+  number: number | string;
   [key: string]: number | string;
 }
 
@@ -15,40 +15,37 @@ interface Student {
   styleUrls: ['./student.component.css']
 })
 export class StudentComponent implements OnInit {
-  students: Student[] = student
+  students: Student[] = student;
+  studentsearch = '';
+  selecttypevalue = '';
+  foundStudents: Student[] = []; // New array to store matched student details
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-    
-  }
-  
-  
-  
-  studentsearch=''
-  selecttypevalue=''
+  ngOnInit(): void {}
+
   studentPresent(): boolean {
     if (this.studentsearch.trim() === '' || this.selecttypevalue.trim() === '') {
       // If the search term or select type value is empty, consider all students present
+      this.foundStudents = [];
       return false;
     }
-  
+
     const lowerCaseSearchTerm = this.studentsearch.toLowerCase();
     const selectedProperty = this.selecttypevalue.trim();
-  
-    return this.students.some((student) => {
+
+    this.foundStudents = this.students.filter((student) => {
       const propertyValue = student[selectedProperty];
-  
+      
+
       // Type guard: Check if the propertyValue is a string and then compare it
       if (typeof propertyValue === 'string') {
         return propertyValue.toLowerCase() === lowerCaseSearchTerm;
       }
-  
+
       return false;
     });
+
+    return this.foundStudents.length > 0;
   }
-  
-  
-  
-  
 }
